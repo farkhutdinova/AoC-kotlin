1516f3013473d96a89adf02508ab5e221264c006
farkhutdinova <evg.farkhutdinova@gmail.com>
2023-12-18 10:03:20 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,45 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    val words: Map<String, Int> = mapOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9
+    )
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun calibrationValue(row: String) : Int =
+        "${row.find { it.isDigit() }}${row.last { it.isDigit() }}".toInt()
 
-    val input = readInput("Day01")
+    fun part1(input: List<String>): Int =
+        input.sumOf { calibrationValue(it) }
+
+    fun part2(input: List<String>): Int =
+        input.sumOf { row ->
+            calibrationValue(
+                    row.mapIndexedNotNull { index, c ->
+                        if (c.isDigit()) c
+                        else
+                            row.possibleWordsAt(index).firstNotNullOfOrNull { candidate ->
+                                words[candidate]
+                            }
+                    }.joinToString()
+            )
+        }
+
+    val testInput = readInput("input/Day01_test1")
+    check(part1(testInput) == 142)
+
+    val input = readInput("input/Day01_task1")
     part1(input).println()
     part2(input).println()
 }
+
+private fun String.possibleWordsAt(staringAt: Int) : List<String> =
+        (3..5).map { len ->
+            substring(staringAt, (staringAt + len).coerceAtMost(length))
+        }
